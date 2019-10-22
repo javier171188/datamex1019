7e8fa68b56a105c55d7b3f8945b662bac2314d39
javier171188 <javier171188@hotmail.com>
2019-10-21 20:05:27 -0500

    conflicto

---
+++ module-1/lab-resolving-git-conflicts/your-code/robin_hood.py
@@ -0,0 +1,10 @@
+# Robin Hood
+import os
+os.chdir("/Users/valeriajimeno/Desktop")
+print(os.getcwd())
+points = [(4, 5), (-0, 2), (4, 7), (1, -3), (3, -2), (4, 5),(3, 2), (5, 7), (-5, 7), (2, 2), (-4, 5), (0, -2),(-4, 7), (-1, 3), (-3, 2), (-4, -5), (-3, 2),(5, 7), (5, 7), (2, 2), (9, 9), (-8, -9)]
+# 1. Robin Hood is famous for hitting an arrow with another arrow. Did you get it?
+'''
+from collections import Counter
+c = Counter()
+for 
