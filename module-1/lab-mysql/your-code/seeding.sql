a514e282c8499df8542831fc24adb28de0fcf96c
javier171188 <javier171188@hotmail.com>
2019-10-28 19:44:40 -0600

    lab-mysql

---
+++ module-1/lab-mysql/your-code/seeding.sql
@@ -0,0 +1,23 @@
+INSERT INTO autos VALUES('Tiguan', 1, 'Volkswagen', 'Tiguan', '2019-01-01', 'azul', 454990, '2019-02-20', 'Raul Lopez', 'Pedro Sola');
+INSERT INTO autos VALUES('Harvy', 2, 'Peugeot', 'Rifter', '2019-01-01', 'green', 500990, '2019-06-17', 'Maria Estrada', 'Arturo Morales');
+INSERT INTO autos VALUES('Pechan', 3, 'Ford', 'Fusion', '2018-01-01', 'black', 300000, '2019-01-23', 'Antonio Ramires', 'Arturo Morales');
+INSERT INTO autos VALUES('batimovil', 4, 'Toyota', 'RAV4', '2018-01-01', 'black', 310000, '2018-11-10', 'Jorge García', 'Sandra Ortiz');
+INSERT INTO autos VALUES('Volvo', 5, 'Toyota', 'V60', '2019-01-01', 'gray', 310000, '2019-10-23', 'Raul Lopez', 'Sandra Ortiz');
+
+INSERT INTO ventas VALUES('Tiguan', 'Antonio Ramires', 'Raul Lopez', '2019-02-20', 454990 ,100000 )
+INSERT INTO ventas VALUES('Rifter', 'Maria Estrada', 'Arturo Morales', '2019-06-17', 500990 ,100000 )
+INSERT INTO ventas VALUES('Fusion', 'Antonio Ramires', 'Arturo Morales', '2019-01-23', 300000 ,100000 )
+INSERT INTO ventas VALUES('RAV4', 'Jorge García', 'Sandra Ortiz', '2018-11-10', 310000 ,105000 )
+INSERT INTO ventas VALUES('V60', 'Raul Lopez', 'Sandra Ortiz', '2019-10-23', 310000 ,105000 )
+
+INSERT INTO clientes VALUES('Raul Lopez', 5553091842, 'raul@gmail.com', 'Paris, ratatui 122, Francia', 'Tiguan', 'Pedro Sola', '1977-10-12', '2019-02-20');
+INSERT INTO clientes VALUES('Raul Lopez', 5553091842, 'raul@gmail.com', 'Paris, ratatui 122, Francia', 'V60', 'Sandra Ortiz', '1977-10-12', '2019-10-23');
+INSERT INTO clientes VALUES('Maria Estrada', 5556894678, 'maries@hotmail.com', 'Puebla 123, Roma Norte CDMX', 'Rifter', 'Arturo Morales', '1985-09-01', '2019-06-17');
+INSERT INTO clientes VALUES('Antonio Ramires', 5555890243, 'Tonoel@yahoo.com', 'Anacahuita 187, Pedregal, Sto. Domingo CDMX', 'Fusion', 'Arturo Morales', '1966-09-01', '2019-01-23');
+INSERT INTO clientes VALUES('Jorge García', 22475890257, 'jorge01@gmail.com', 'calle 12, Polanco CDMX', 'RAV4', 'Sandra Ortiz', '1995-03-05', '2018-11-10');
+
+INSERT INTO vendedores VALUES('Pedro Sola', '1950-07-23', 1, 100000, 1, '2019-02-20')
+INSERT INTO vendedores VALUES('Arturo Morales', '1999-02-27', 2, 200000, 2, '2019-01-23')
+INSERT INTO vendedores VALUES('Sandra Ortiz', '1997-12-12', 2, 210000, 3, '2019-10-23')
+
+
