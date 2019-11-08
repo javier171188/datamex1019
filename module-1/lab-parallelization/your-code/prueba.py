b3b9c4e654d0e2c271c95836efd6b002e130f205
javier171188 <javier171188@hotmail.com>
2019-11-07 18:15:27 -0600

    lab-parappelization

---
+++ module-1/lab-parallelization/your-code/prueba.py
@@ -0,0 +1,44 @@
+{
+ "cells": [
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "metadata": {},
+   "outputs": [],
+   "source": [
+    "import multiprocessing\n",
+    "def square(x):  \n",
+    "    return x*x\n",
+    "\n",
+    "data = [x for x in range(100)]\n",
+    "pool = multiprocessing.Pool()\n",
+    "result = pool.map(square, data)\n",
+    "pool.join()\n",
+    "pool.terminate()\n",
+    "\n",
+    "print(result)"
+   ]
+  }
+ ],
+ "metadata": {
+  "kernelspec": {
+   "display_name": "Python 3",
+   "language": "python",
+   "name": "python3"
+  },
+  "language_info": {
+   "codemirror_mode": {
+    "name": "ipython",
+    "version": 3
+   },
+   "file_extension": ".py",
+   "mimetype": "text/x-python",
+   "name": "python",
+   "nbconvert_exporter": "python",
+   "pygments_lexer": "ipython3",
+   "version": "3.7.1"
+  }
+ },
+ "nbformat": 4,
+ "nbformat_minor": 2
+}
